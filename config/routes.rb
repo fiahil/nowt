b116b5e53011f3561ea53c60acf1c4f108fc30a1
Eric Kim <erickim928@gmail.com>
2014-02-24 16:15:40 -0800

    updated routes file (for users/profile)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Nowt::Application.routes.draw do
+  get 'users/profile'
   devise_for :users, controllers: {registrations: 'registrations'}
 
   root 'home#index'
 
   resources :posts
   resources :users, only: [:show, :index]
+
+  
 end
