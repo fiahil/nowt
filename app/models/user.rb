c15433820c48733936fa24d86081099070bc44aa
Fiahil <fiahil@gmail.com>
2014-03-01 12:45:25 -0800

    fix email adress instead of account name for facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
       user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name
+      user.name = auth.info.email
     end
   end
 
