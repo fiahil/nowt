815a2b26b1d593063ac8716ed4a63eb12d7db5fc
Eric Kim <erickim928@gmail.com>
2014-03-03 02:25:49 -0800

    Modified home page and added some additional routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,14 @@ Nowt::Application.routes.draw do
     delete "/logout" => "devise/sessions#destroy"
     get "/profile" => "users#profile"
     get "/edit" => "registrations#edit"
-
   end
 
 
   devise_for :users, controllers: {registrations: 'registrations', :omniauth_callbacks => 'omniauth_callbacks'}
 
   resources :posts 
+  get '/posts/new', to: "posts#new"
+
   get '/board', to: "posts#index", as: "board"
 
   resources :users, only: [:show, :index]
