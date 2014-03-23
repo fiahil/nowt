84cca392ba9864cd7a442d88733cbab378de450d
erickim92891 <erickim928@gmail.com>
2014-03-22 18:29:10 -0700

    Updated db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140322190643) do
+ActiveRecord::Schema.define(version: 20140322194230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 20140322190643) do
 
   add_index "tags", ["description"], name: "index_tags_on_description", using: :btree
   add_index "tags", ["label"], name: "index_tags_on_label", using: :btree
+
+  create_table "user_tags", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "tag_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
