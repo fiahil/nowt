83d9084180654e7b2a27d5402c1378dcafcf3242
dungtienta <dung.tien.ta@gmail.com>
2014-03-24 21:18:25 -0700

    refactored task controller, remove checks, check input in js instead

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,8 @@
 class TasksController < ApplicationController
 	
 	def live_search
-	  input = params[:q]
-
-	  #get input, check for empty 
-	  if(input != "")
-	  	@tasks = Tag.where("name ilike ? or description ilike ?", "%#{params[:q]}%", "%#{params[:q]}%").limit(15)
-	  else
-	  	@tasks = Tag.where("name like 'NOTHING'")
-	  end
-
-	  render :layout => false
+	 	@tasks = Tag.where("name ilike ? or description ilike ?", "%#{params[:q]}%", "%#{params[:q]}%").limit(15)
+	  	render :layout => false
 	end
 
 	#if empty
