6b9efdb539d52b7a7b01ba60c071b3ec2ac8e9cd
Eric Kim <erickim928@gmail.com>
2014-04-10 05:00:05 -0700

    index page will show ALL posts regardless of whether or not the user is signed in now

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,7 @@ class PostsController < ApplicationController
 
   # GET /posts
   def index
-    if user_signed_in?
-      @posts = find_interests
-    else
       @posts = Post.all
-    end
-
   end
   
   # GET /posts/1
@@ -63,7 +58,7 @@ class PostsController < ApplicationController
     end
     render action: "index"
   end
-
+  
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_post
