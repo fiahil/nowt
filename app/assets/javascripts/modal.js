c482f96b7ed1e517f74c0e0c4417bf6532473cef
Eric Kim <erickim928@gmail.com>
2014-04-13 21:17:21 -0700

    Modified the look of the nowt modal. I also added nowt types to the modal

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,57 +1,7 @@
-$(function(){ //For Date Clicked
-    $( "#startDate" ).datepicker();
-	$( "#endDate" ).datepicker();
-  });
-
-function handleSelection(choice) 
-{
-	document.getElementById('bottomRow').style.display="none";
-	document.getElementById('topForms').style.display="none";
-	document.getElementById('toolTip').style.display="block";
-	document.getElementById('author').style.display="block";
-	document.getElementById('location').style.display="block";
-	document.getElementById('classNumber').style.display="block";
-	document.getElementById('conditionPrice').style.display="block";
-	document.getElementById('timeRow').style.display="block";
-	document.getElementById('dateRow').style.display="block";
-	
-	
-	if(choice=='selectBook'||choice=='selectMeet'||choice=='selectService'||choice=='selectEvent')
-	{
-		document.getElementById('topForms').style.display="block";
-		document.getElementById('bottomRow').style.display="block";
-		document.getElementById('toolTip').style.display="none";
-	}
-	else
-	{
-		document.getElementById('topForms').style.display="none";
-		document.getElementById('bottomRow').style.display="none";
-		document.getElementById('toolTip').style.display="block";
-	}
-	
-if(choice=='selectBook')
-	{
-		document.getElementById('location').style.display="none";
-		document.getElementById('timeRow').style.display="none";
-		document.getElementById('dateRow').style.display="none";
-	}
-	else if(choice=='selectMeet')
-	{
-		document.getElementById('author').style.display="none";
-		document.getElementById('conditionPrice').style.display="none";
-	}
-	else if(choice=='selectService')
-	{
-		document.getElementById('author').style.display="none";
-		document.getElementById('classNumber').style.display="none";
-		document.getElementById('conditionPrice').style.display="none";
-		document.getElementById('timeRow').style.display="none";
-
-	}
-	else //selectEvent
-	{
-		document.getElementById('author').style.display="none";
-		document.getElementById('conditionPrice').style.display="none";
-		document.getElementById('classNumber').style.display="none";
-	}
-}
+$(document).ready(function(){
+	$('.cbp-vimenu > li a').click(function(){
+		$('.cbp-vimenu > li a').removeClass("modal-category-active");
+		$(this).addClass("modal-category-active");
+		$('#post_category').val($(this).attr('title')
+) 	});
+});
