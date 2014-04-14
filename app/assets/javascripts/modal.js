35b6c7c4940d69b04eaf4285204f360a2aa4ebf1
Eric Kim <erickim928@gmail.com>
2014-04-14 06:13:59 -0700

    Removed the weird https link that was inside the modal.js file

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('.cbp-vimenu > li a').click(function(){http://svn.cometd.org/trunk/bayeux/bayeux.html
+	$('.cbp-vimenu > li a').click(function(){
 		$('.cbp-vimenu > li a').removeClass("modal-category-active");
 		$(this).addClass("modal-category-active");
 		$('#post_category').val($(this).attr('title')); 	
