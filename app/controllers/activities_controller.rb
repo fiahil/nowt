e72e671c16332ebb781030041a0a99e94283915c
Eric Kim <erickim928@gmail.com>
2014-04-16 05:33:03 -0700

    Modified activities page to show both when someone creates a post that you subscribed to AND when another user comments on one of your posts

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,17 @@
 class ActivitiesController < ApplicationController
   def index
-  	@activities = PublicActivity::Activity.order("created_at desc").where(trackable_id: find_interests, owner_type: "User").where(['owner_id <> ?', current_user]).page(params[:page]).per(10)
+  	@activities = find_comment_activities
 
   end
+
+  private
+
+  	def find_comment_activities 
+  		my_posts = Post.where(user_id: current_user).ids
+  		activities = PublicActivity::Activity.where(key: "post.comment").where(trackable_id: my_posts).where(['owner_id <> ?', current_user]).ids
+  		activities += PublicActivity::Activity.where(trackable_id: find_interests, owner_type: "User").where(['owner_id <> ?', current_user]).ids
+		
+		return PublicActivity::Activity.where(id: activities.uniq).page(params[:page]).per(10)
+	end
+
 end
